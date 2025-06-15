a0c1dee0e0e65a5213147a7ffa700da4cac53f1d
Stvlutta <stvlutta@gmail.com>
2025-06-15 21:09:54 +0300

    Add Restaurant model
    - Create Restaurant SQLAlchemy model with name and address fields
    - Add relationship to RestaurantPizza for many-to-many with Pizza
    - Include serialization methods for JSON responses

---
+++ server/models/restaurant.py
@@ -0,0 +1,25 @@
+from server.app import db
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant', cascade='all, delete-orphan')
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address
+        }
+    
+    def to_dict_with_pizzas(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address,
+            'restaurant_pizzas': [rp.to_dict() for rp in self.restaurant_pizzas]
+        }
